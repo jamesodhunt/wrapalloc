fdba0d43829df6da8da1fa924a6b632a047fcc95
James Hunt <jamesodhunt@ubuntu.com>
2015-10-03 18:43:07 +0100

    Update email address.
    Signed-off-by: James Hunt <jamesodhunt@ubuntu.com>

--- tests/check_wrapalloc.c
+++ tests/check_wrapalloc.c
@@ -1,7 +1,7 @@
 /*--------------------------------------------------------------------
  * wrapalloc - check unit test program (see http://check.sourceforge.net/).
  *
- * Copyright (C) 2012-2015 James Hunt <james.hunt@ubuntu.com>.
+ * Copyright (C) 2012-2015 James Hunt <jamesodhunt@ubuntu.com>.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
