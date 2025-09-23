bb680267d6b101d3fe86d73cfb7d808091a966ab
James O. D. Hunt <jamesodhunt@gmail.com>
2025-09-23 15:27:54 +0100

    chore: Update more copyrights
    Signed-off-by: James O. D. Hunt <jamesodhunt@gmail.com>

--- src/wrap_alloc.h
+++ src/wrap_alloc.h
@@ -1,7 +1,7 @@
 /*--------------------------------------------------------------------
  * wrapalloc - memory allocation debugging library header.
  *
- * Copyright (C) 2011-2015 James Hunt <jamesodhunt@gmail.com>.
+ * Copyright (C) 2011-2025 James Hunt <jamesodhunt@gmail.com>.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
